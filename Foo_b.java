12898a46ea8c3d940fa8fd6db01a9cfc406fb82e
wanyao <wanyaocsg17@icloud.com>
2025-01-06 15:43:45 +0800

    Delete Foo_b.java

--- Foo_b.java
+++
@@ -1,8 +0,0 @@
-public class Foo{
-    public void foo(){
-        System.out.println("unchanged");
-        System.out.println("unchanged");
-        int t = 2;
-        System.out.println("original");
-    }
-}
